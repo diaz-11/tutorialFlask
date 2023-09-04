fb16017d8a71673102997f91aeff07d54129b9b0
lukabazan232 <lukabazan404@gmail.com>
2023-09-04 12:25:14 -0300

    email

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -11,9 +11,11 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
-    if request.method == 'POST':
+    if request.method == 'POST': 
         username = request.form['username']
         password = request.form['password']
+        confirmaPass = request.form['confirPass']
+        email = request.form ['email']
         db = get_db()
         error = None
 
@@ -21,12 +23,15 @@ def register():
             error = 'Username is required.'
         elif not password:
             error = 'Password is required.'
-
+        elif not confirmaPass == password:
+            error = 'Contraseñas no coinciden.'
+        elif not email:
+            error = 'email incorrecto'
         if error is None:
             try:
                 db.execute(
-                    "INSERT INTO user (username, password) VALUES (?, ?)",
-                    (username, generate_password_hash(password)),
+                    "INSERT INTO user (username, email, password) VALUES (?,?,?)",
+                    (username, email,generate_password_hash(password)),
                 )
                 db.commit()
             except db.IntegrityError:
